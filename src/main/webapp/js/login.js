1260ca6b13aaeccfe650583c08a204c52343e7a1
ByronBecker <byron.becker@colorado.edu>
2017-12-08 18:22:56 -0700

    remapped project hierarchy

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,10 @@
+
+function login()
+	{
+		let username = document.getElementsByName('username')[0].value;
+		let password = document.getElementsByName('password')[0].value;
+		//let name = username.value;
+		console.log(username);
+		//console.log(name);
+		//alert(user);
+	}
