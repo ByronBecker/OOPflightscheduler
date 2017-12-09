73948d41cd1a09c2c14d1ca5a4d186a5497704ac
ByronBecker <byron.becker@colorado.edu>
2017-12-09 12:38:16 -0700

    about to merge in Flight branch

---
+++ src/main/java/com/flight_scheduler/AirportAdmin.java
@@ -0,0 +1,37 @@
+package com.flight_scheduler;
+
+public class AirportAdmin {
+	private int user_id = 0;
+	private int number_gates;
+	
+	public AirportAdmin() {
+
+	}
+
+	public boolean changeFlightStatus(Flight flight, boolean status) {
+
+	}
+
+	public boolean assignFlightPriority(Flight flight, int priority) {
+
+	}
+
+	public void assignAirlinePriority(String airline, int priority) {
+
+	}
+
+	public boolean approveFlightRequest(FlightInfo requested_flight, String explanation) {
+
+	}
+
+	public boolean confirm() {
+
+	}
+
+	public void setNumberOfGates(int number_gates) {
+		this.number_gates = number_gates;
+	}
+
+	
+
+}
