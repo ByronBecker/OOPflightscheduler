7d9190ee0e4be646e0bd41edba612b2ceb0b86e3
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-12-10 20:58:51 -0700

    fixed Controller

--- src/main/java/com/flight_scheduler/Controller.java
+++ src/main/java/com/flight_scheduler/Controller.java
@@ -1,34 +1,35 @@
-//
-///**
-// * @author raghavsharma
-// *
-// */
-//public class Controller {
-//	public boolean login(String userName, String password) {
-//
-//	}
-//	public void logout() {
-//
-//	}
-//	public boolean openGateExists(LocalDateTime early_time, LocalDateTime late_time) {
-//
-//	}
-//	public Flight requestFlight(FlightInfo requested_flight) {
-//
-//	}
-//	public boolean checkPriority(FlightInfo requested_flight) {
-//
-//	}
-//	public Flight emptyFlight() {
-//
-//	}
-//	public void receiveTimeWindow(LocalDateTime early_time, LocalDateTime late_time ) {
-//
-//	}
-//	public void receiveFlight(int flight_number) {
-//
-//	}
-//	public void receiveAirline(String airline) {
-//
-//	}
-//}
+import com.flight_scheduler.*;
+import java.time.*;
+/**
+ * @author raghavsharma
+ *
+ */
+public class Controller {
+	public boolean login(String userName, String password) {
+		return false;
+	}
+	public void logout() {
+
+	}
+	public boolean openGateExists(LocalDateTime early_time, LocalDateTime late_time) {
+		return false;
+	}
+	public Flight requestFlight(FlightInfo requested_flight) {
+		return new Flight();
+	}
+	public boolean checkPriority(FlightInfo requested_flight) {
+		return false;
+	}
+	public Flight emptyFlight() {
+		return new Flight();
+	}
+	public void receiveTimeWindow(LocalDateTime early_time, LocalDateTime late_time ) {
+
+	}
+	public void receiveFlight(int flight_number) {
+
+	}
+	public void receiveAirline(String airline) {
+
+	}
+}
