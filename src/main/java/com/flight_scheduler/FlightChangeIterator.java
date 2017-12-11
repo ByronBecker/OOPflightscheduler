4e61c10cdb14044cddbd089507f563ae04f68c99
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-12-10 20:30:22 -0700

    Add Iterator Interface

--- src/main/java/com/flight_scheduler/FlightChangeIterator.java
+++ src/main/java/com/flight_scheduler/FlightChangeIterator.java
@@ -1,9 +1,10 @@
+import java.time.*;
+import com.flight_scheduler.Flight;
+public interface FlightChangeIterator {
 
-//public interface FlightChangeIterator {
-//
-//	public boolean cancelFlightsByNumber(int flight_number);
-//
-//	public boolean cancelFlightsByTimeWindow(LocalDateTime early_time, LocalDateTime late_time, String airline);
-//
-//	//public Flight addFlight(Flight flight);
-//}
+	public boolean cancelFlightsByNumber(int flight_number);
+
+	public boolean cancelFlightsByTimeWindow(LocalDateTime early_time, LocalDateTime late_time, String airline);
+
+	public Flight addFlight(Flight flight);
+}
